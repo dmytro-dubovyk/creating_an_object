ad815956ea8736e03c89a6d69f6bb6eeba9fce64
Dmytro Dubovyk <dmytro.dubovyk@globallogic.com>
2018-11-22 15:51:56 +0200

    Add move constructor
    Signed-off-by: Dmytro Dubovyk <dmytro.dubovyk@globallogic.com>

--- src/constructors.cpp
+++ src/constructors.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
+#include <utility>
 
 struct A {
-    A() { p = new int(10); }
-    ~A() { delete p; }
-
     int i;
     int j;
     int* p;
+    
+    A() { p = new int(10); }
+
+    A(const A& a)
+      : i(a.i), j(a.j), p(new int(*a.p)) { std::cout << "Copy constructor of A" << std::endl; }
+
+    A(A&& a)
+      : i(std::exchange(a.i, 0)), j(std::exchange(a.j, 0)), p(std::exchange(a.p, nullptr)) {
+        std::cout << "Move constructor of A" << std::endl;
+    }
+
+    A& operator=(A&& a) {
+        i = std::exchange(a.i, 0);
+        j = std::exchange(a.j, 0);
+        p = std::exchange(a.p, nullptr);
+        return *this;
+    }
+
+    ~A() { delete p; }
 };
 
 struct B {
@@ -32,6 +49,8 @@ private:
 int main() {
     for(int i = 0; i < 100; ++i) {
         A* pa = new A();
+        A a(*pa);
+        A a1{std::move(a)};
         B* pb = new B();
         //C c{2.5}; // error! implicit conversion float -> int
         C c{10};
